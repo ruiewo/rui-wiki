09b0e59af00fe059955da975619c4d7a3e734ceb
t.wada <109674512+ruiewo@users.noreply.github.com>
2023-12-12 23:50:30 +0900

    add side menu.

--- src/pages/main.ts
+++ src/pages/main.ts
@@ -1,3 +1,29 @@
+import { createSideMenu } from "../conponents/sideMenu/sideMenu";
+
 export const MainPage = {
-  load: (data: any) => {},
+  load: (contents: Contents) => {
+    const app = document.getElementById("app")!;
+
+    const layout = createLayout();
+    const sidebar = createSideMenu(contents.setting, contents.articles);
+    // const contentsDom = createContents();
+    layout.appendChild(sidebar);
+    // layout.appendChild(contentsDom);
+
+    app.appendChild(layout);
+  },
 };
+
+type Contents = {
+  setting: {
+    title: string;
+    subTitle: string;
+  };
+  articles: string[];
+};
+
+function createLayout() {
+  const layout = document.createElement("div");
+  layout.classList.add("layout");
+  return layout;
+}
